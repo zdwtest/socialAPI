8606669eb24f5d0019139995da7a97fc8a22a6d0
kalijerry <729913310@qq.com>
2024-05-14 10:07:10 +0800

    May14

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from peewee import CharField, IntegerField, ForeignKeyField, TextField, DateTime
 class User(db.Model):
     """用户模型"""
     username = CharField(unique=True)  # 用户名，唯一
-    password = CharField()  # 密码
+    password = CharField()  # 密码  此功能在测试完毕后用hash进行验证
     email = CharField(unique=True)  # 邮箱，唯一
     age = IntegerField(null=True)  # 年龄，可以为空
 
@@ -18,6 +18,9 @@ class Tweet(db.Model):
     """推文模型"""
     user = ForeignKeyField(User, backref='tweets')  # 关联用户模型，反向关系为 'tweets'
     content = TextField()  # 推文内容
+    tag = TextField()  # 标签
+    like = CharField()  # 点赞数
+    view = CharField()  # 观看数
     created_at = DateTimeField(default=datetime.datetime.now)  # 创建时间
 
 
--- test/sqlTest.py
+++ test/sqlTest.py
@@ -4,4 +4,4 @@ from app.models import User, Tweet
 #user = User.create(username='john1', password='secret', email='john1@example.com', age=30)
 
 # 创建推文
-tweet = Tweet.create(user_id="john1", content='Hello, world! TEST1')
+tweet = Tweet.create(user_id="john1", content='Hello, world! TEST1',tag="test",like="0",view="0")
